75b6072c0fa73707a2c245858ac1de5c1c7b98c3
Jany Muong <me.roumuong@gmail.com>
2024-02-18 21:43:40 +0300

    checkboxes; more radio buttons

--- ObjectDemonstrationsForm.Designer.vb
+++ ObjectDemonstrationsForm.Designer.vb
@@ -29,6 +29,14 @@ Partial Class ObjectDemonstrationsForm
         GaramondRadioButton = New RadioButton()
         MagnetoRadioButton = New RadioButton()
         TahomaRadioButton = New RadioButton()
+        GreenRadioButton = New RadioButton()
+        RedRadioButton = New RadioButton()
+        BlueRadioButton = New RadioButton()
+        Label1 = New Label()
+        Label2 = New Label()
+        ItalicCheckBox = New CheckBox()
+        BoldCheckBox = New CheckBox()
+        BoldItalicCheckBox = New CheckBox()
         SuspendLayout()
         ' 
         ' ObjectDemoHeaderLabel
@@ -43,7 +51,7 @@ Partial Class ObjectDemonstrationsForm
         ' 
         ' DisplayTestTextBox
         ' 
-        DisplayTestTextBox.Location = New Point(23, 155)
+        DisplayTestTextBox.Location = New Point(73, 146)
         DisplayTestTextBox.Name = "DisplayTestTextBox"
         DisplayTestTextBox.Size = New Size(184, 31)
         DisplayTestTextBox.TabIndex = 1
@@ -53,7 +61,7 @@ Partial Class ObjectDemonstrationsForm
         ' 
         DisplayTestLabel.AutoSize = True
         DisplayTestLabel.BorderStyle = BorderStyle.Fixed3D
-        DisplayTestLabel.Location = New Point(57, 125)
+        DisplayTestLabel.Location = New Point(107, 116)
         DisplayTestLabel.Name = "DisplayTestLabel"
         DisplayTestLabel.Size = New Size(112, 27)
         DisplayTestLabel.TabIndex = 2
@@ -63,7 +71,7 @@ Partial Class ObjectDemonstrationsForm
         ' 
         FontLabel.AutoSize = True
         FontLabel.BorderStyle = BorderStyle.Fixed3D
-        FontLabel.Location = New Point(280, 125)
+        FontLabel.Location = New Point(384, 116)
         FontLabel.Name = "FontLabel"
         FontLabel.Size = New Size(50, 27)
         FontLabel.TabIndex = 3
@@ -72,7 +80,7 @@ Partial Class ObjectDemonstrationsForm
         ' GaramondRadioButton
         ' 
         GaramondRadioButton.AutoSize = True
-        GaramondRadioButton.Location = New Point(280, 157)
+        GaramondRadioButton.Location = New Point(384, 148)
         GaramondRadioButton.Name = "GaramondRadioButton"
         GaramondRadioButton.Size = New Size(121, 29)
         GaramondRadioButton.TabIndex = 5
@@ -83,7 +91,7 @@ Partial Class ObjectDemonstrationsForm
         ' MagnetoRadioButton
         ' 
         MagnetoRadioButton.AutoSize = True
-        MagnetoRadioButton.Location = New Point(407, 157)
+        MagnetoRadioButton.Location = New Point(511, 148)
         MagnetoRadioButton.Name = "MagnetoRadioButton"
         MagnetoRadioButton.Size = New Size(109, 29)
         MagnetoRadioButton.TabIndex = 6
@@ -94,7 +102,7 @@ Partial Class ObjectDemonstrationsForm
         ' TahomaRadioButton
         ' 
         TahomaRadioButton.AutoSize = True
-        TahomaRadioButton.Location = New Point(522, 157)
+        TahomaRadioButton.Location = New Point(626, 148)
         TahomaRadioButton.Name = "TahomaRadioButton"
         TahomaRadioButton.Size = New Size(99, 29)
         TahomaRadioButton.TabIndex = 7
@@ -102,11 +110,105 @@ Partial Class ObjectDemonstrationsForm
         TahomaRadioButton.Text = "Tahoma"
         TahomaRadioButton.UseVisualStyleBackColor = True
         ' 
+        ' GreenRadioButton
+        ' 
+        GreenRadioButton.AutoSize = True
+        GreenRadioButton.Location = New Point(626, 251)
+        GreenRadioButton.Name = "GreenRadioButton"
+        GreenRadioButton.Size = New Size(83, 29)
+        GreenRadioButton.TabIndex = 8
+        GreenRadioButton.TabStop = True
+        GreenRadioButton.Text = "Green"
+        GreenRadioButton.UseVisualStyleBackColor = True
+        ' 
+        ' RedRadioButton
+        ' 
+        RedRadioButton.AutoSize = True
+        RedRadioButton.Location = New Point(626, 321)
+        RedRadioButton.Name = "RedRadioButton"
+        RedRadioButton.Size = New Size(67, 29)
+        RedRadioButton.TabIndex = 9
+        RedRadioButton.TabStop = True
+        RedRadioButton.Text = "Red"
+        RedRadioButton.UseVisualStyleBackColor = True
+        ' 
+        ' BlueRadioButton
+        ' 
+        BlueRadioButton.AutoSize = True
+        BlueRadioButton.Location = New Point(626, 286)
+        BlueRadioButton.Name = "BlueRadioButton"
+        BlueRadioButton.Size = New Size(70, 29)
+        BlueRadioButton.TabIndex = 10
+        BlueRadioButton.TabStop = True
+        BlueRadioButton.Text = "Blue"
+        BlueRadioButton.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BorderStyle = BorderStyle.Fixed3D
+        Label1.Location = New Point(611, 221)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(98, 27)
+        Label1.TabIndex = 11
+        Label1.Text = "Font Color"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BorderStyle = BorderStyle.Fixed3D
+        Label2.Location = New Point(451, 221)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(92, 27)
+        Label2.TabIndex = 12
+        Label2.Text = "Font Style"
+        ' 
+        ' ItalicCheckBox
+        ' 
+        ItalicCheckBox.AutoSize = True
+        ItalicCheckBox.Font = New Font("Segoe UI", 9F, FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        ItalicCheckBox.Location = New Point(451, 252)
+        ItalicCheckBox.Name = "ItalicCheckBox"
+        ItalicCheckBox.Size = New Size(77, 29)
+        ItalicCheckBox.TabIndex = 13
+        ItalicCheckBox.Text = "Italic"
+        ItalicCheckBox.UseVisualStyleBackColor = True
+        ' 
+        ' BoldCheckBox
+        ' 
+        BoldCheckBox.AutoSize = True
+        BoldCheckBox.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BoldCheckBox.Location = New Point(451, 287)
+        BoldCheckBox.Name = "BoldCheckBox"
+        BoldCheckBox.Size = New Size(77, 29)
+        BoldCheckBox.TabIndex = 14
+        BoldCheckBox.Text = "Bold"
+        BoldCheckBox.UseVisualStyleBackColor = True
+        ' 
+        ' BoldItalicCheckBox
+        ' 
+        BoldItalicCheckBox.AutoSize = True
+        BoldItalicCheckBox.Font = New Font("Segoe UI", 9F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        BoldItalicCheckBox.Location = New Point(451, 321)
+        BoldItalicCheckBox.Name = "BoldItalicCheckBox"
+        BoldItalicCheckBox.Size = New Size(126, 29)
+        BoldItalicCheckBox.TabIndex = 15
+        BoldItalicCheckBox.Text = "Bold Italic"
+        BoldItalicCheckBox.UseVisualStyleBackColor = True
+        ' 
         ' ObjectDemonstrationsForm
         ' 
         AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(800, 508)
+        Controls.Add(BoldItalicCheckBox)
+        Controls.Add(BoldCheckBox)
+        Controls.Add(ItalicCheckBox)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(BlueRadioButton)
+        Controls.Add(RedRadioButton)
+        Controls.Add(GreenRadioButton)
         Controls.Add(TahomaRadioButton)
         Controls.Add(MagnetoRadioButton)
         Controls.Add(GaramondRadioButton)
@@ -127,4 +229,13 @@ Partial Class ObjectDemonstrationsForm
     Friend WithEvents GaramondRadioButton As RadioButton
     Friend WithEvents MagnetoRadioButton As RadioButton
     Friend WithEvents TahomaRadioButton As RadioButton
+    Friend WithEvents GreenRadioButton As RadioButton
+    Friend WithEvents RedRadioButton As RadioButton
+    Friend WithEvents BlueRadioButton As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ItalicCheckBox As CheckBox
+    Friend WithEvents CheckBox As CheckBox
+    Friend WithEvents BoldItalicCheckBox As CheckBox
+    Friend WithEvents BoldCheckBox As CheckBox
 End Class
--- ObjectDemonstrationsForm.vb
+++ ObjectDemonstrationsForm.vb
@@ -32,5 +32,78 @@
         End If
     End Sub
 
+    Private Sub GreenRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles GreenRadioButton.CheckedChanged
+        If GreenRadioButton.Checked Then
+            DisplayTestTextBox.ForeColor = Color.Green
+        End If
+    End Sub
+
+    Private Sub BlueRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles BlueRadioButton.CheckedChanged
+        If BlueRadioButton.Checked Then
+            DisplayTestTextBox.ForeColor = Color.Blue
+        End If
+    End Sub
+
+    Private Sub RedRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles RedRadioButton.CheckedChanged
+        If RedRadioButton.Checked Then
+            DisplayTestTextBox.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub ItalicCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles ItalicCheckBox.CheckedChanged
+        Dim fontStyle As FontStyle = FontStyle.Regular
+
+        If ItalicCheckBox.Checked Then
+            fontStyle = fontStyle Or FontStyle.Italic
+        End If
+
+        If BoldCheckBox.Checked Then
+            fontStyle = fontStyle Or FontStyle.Bold
+        End If
+
+        If BoldItalicCheckBox.Checked Then
+            fontStyle = fontStyle Or FontStyle.Bold Or FontStyle.Italic
+        End If
+
+        DisplayTestTextBox.Font = New Font(DisplayTestTextBox.Font, fontStyle)
+    End Sub
+
+    Private Sub BoldCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles BoldCheckBox.CheckedChanged
+        Dim fontStyle As FontStyle = FontStyle.Regular
+
+        If ItalicCheckBox.Checked Then
+            fontStyle = fontStyle Or FontStyle.Italic
+        End If
+
+        If BoldCheckBox.Checked Then
+            fontStyle = fontStyle Or FontStyle.Bold
+        End If
+
+        If BoldItalicCheckBox.Checked Then
+            fontStyle = fontStyle Or FontStyle.Bold Or FontStyle.Italic
+        End If
+
+        DisplayTestTextBox.Font = New Font(DisplayTestTextBox.Font, fontStyle)
+    End Sub
+
+    Private Sub BoldItalicCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles BoldItalicCheckBox.CheckedChanged
+        Dim fontStyle As FontStyle = FontStyle.Regular
+
+        If ItalicCheckBox.Checked Then
+            fontStyle = fontStyle Or FontStyle.Italic
+        End If
+
+        If BoldCheckBox.Checked Then
+            fontStyle = fontStyle Or FontStyle.Bold
+        End If
+
+        If BoldItalicCheckBox.Checked Then
+            fontStyle = fontStyle Or FontStyle.Bold Or FontStyle.Italic
+        End If
+
+        DisplayTestTextBox.Font = New Font(DisplayTestTextBox.Font, fontStyle)
+    End Sub
+
+
 
 End Class
