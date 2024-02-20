8ad16f64817aa676cc3f977bab935bc02d65f960
Jany Muong <me.roumuong@gmail.com>
2024-02-20 08:50:18 +0300

    [include] all files

--- ObjectDemonstrationsForm.vb
+++ ObjectDemonstrationsForm.vb
@@ -127,4 +127,8 @@ Public Class ObjectDemonstrationsForm
             ImagePictureBox.Image = selectedImage
         End If
     End Sub
+
+    Private Sub ImagePictureBox_Click(sender As Object, e As EventArgs) Handles ImagePictureBox.Click
+
+    End Sub
 End Class
